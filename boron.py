e385751fef32a7e0659472bf931fdfb3bbe614da
Craig Dupree <craig.m.dupree@gmail.com>
2013-10-20 14:04:29 -0500

    * expand tabs out to 8 (just because vim currently wants to ai   for that), remove them, etc.
    * remove uneeded code from my template
    
    * add Exception class

--- boron.py
+++ boron.py
@@ -12,159 +12,123 @@ import getopt
 
 import re
 
+class ParadeError(Exception):	
+        def __init__(self,msg):
+                self.msg = msg
+
+        def __str__(self):
+                return self.msg 
+
+
+
 class Parade:
 
-	class Node:
-					def __init__(self,name):
-									self.predArr = []
-									self.name = name
-									self.color = 0 # Color 0 => node is white (unvisited)
-									               # Color 1 => node is grey (visited once)
-																 # Color 2 => node is black (sorted)
+        class Node:
+                def __init__(self,name):
+                        self.predArr = []
+                        self.name = name
+                        self.color = 0   # Color 0 => node is white (unvisited)
+                                         # Color 1 => node is grey (visited once)
+                                         ## Color 2 => node is black (sorted)
 
-					def __addpred__(self,name):
-									self.name = name
+                def __addpred__(self,name):
+                        self.name = name
 
-					def __pred__(self,name):
-									self.name = name
+                def __pred__(self,name):
+                        self.name = name
 
-	def __init__(self): 
-		self.graph = {}
-		self.ordering = []
+        def __init__(self): 
+                self.graph = {}
+                self.ordering = []
 
-	def printGraph(self):
-		for key in self.graph.keys():
-			print "%s must come after:" % (key)
-			for node in self.graph[key].predArr:
-				print node.name
+        def printGraph(self):
+                for key in self.graph.keys():
+                        print "%s must come after:" % (key)
+                        for node in self.graph[key].predArr:
+                                print node.name
 
-			print
+                        print
 
-	def sort(self):
-		for key in self.graph.keys():
-			if  self.graph[key].color == 0:
-				self.__visit__(self.graph[key])
+        def sort(self):
+                for key in self.graph.keys():
+                        if  self.graph[key].color == 0:
+                                self.__visit__(self.graph[key])
 
-	def __visit__(self, node):
-		# This node is already sorted
-		if node.color == 2:
-			return
+        def __visit__(self, node):
 
-		# This node is already visited, and 
-		# an ancestor node has lead us back 
-		# to it, so we must have a loop
-		if node.color == 1:
-			print "Graph is not a DAG"
-			sys.exit(1)
+                # This node is already sorted
+                if node.color == 2:
+                        return
 
-		node.color = 1
+                # This node is already visited, and 
+                # an ancestor node has lead us back 
+                # to it, so we must have a loop
+                if node.color == 1:
+                        print "Graph is not a DAG"
+                        sys.exit(1)
 
-		# Visit predecessors
-		for predecessor in node.predArr:
-			self.__visit__(predecessor)
+                node.color = 1
 
-		# Now all predecessors must be
-		# in the ordering, so this node
-		# can be be added.
-		node.color = 2
-		#self.ordering.insert(0,node)
-		self.ordering.append(node)
-		
-	def output(self):
-		for node in self.ordering:
-			print node.name
+                # Visit predecessors
+                for predecessor in node.predArr:
+                        self.__visit__(predecessor)
 
-	def input(self,fname):
-		fin = open(fname)
+                # Now all predecessors must be
+                # in the ordering, so this node
+                # can be be added.
+                node.color = 2
+                self.ordering.append(node)
 
-		for line in fin:
-			# strip newline from the input.
-			line = line.rstrip()
-			# Now split apart the line.  
-			m = re.match("(.*) comes (before|after) (.*)", line)
+        def output(self):
+                for node in self.ordering:
+                        print node.name
 
-			# default direction of arrow in graph
-			head = m.group(1)
-			tail = m.group(3)
+        def input(self,fname):
+                fin = open(fname)
 
-			# Now check to see if the direction above is okay.  For this 
-			# algorithm we have an edge from x to y if x comes after y in 
-			# parade.  Thus, we swap if x comes before y
+                for line in fin:
+                        print line
+                        # strip newline from the input.
+                        line = line.rstrip()
+                        # Now split apart the line.  
+                        m = re.match("(.*) comes (before|after) (.*)", line)
 
-			if m.group(2) == "before":
-				swap = head
-				head = tail
-				tail = swap
+                        # default direction of arrow in graph
+                        head = m.group(1)
+                        tail = m.group(3)
 
-			if not self.graph.__contains__(head):
-				newNode = self.Node(head)
-				self.graph[head] = newNode
+                        # Now check to see if the direction above is okay.  For this 
+                        # algorithm we have an edge from x to y if x comes after y in 
+                        # parade.  Thus, we swap if x comes before y
 
-			if not self.graph.__contains__(tail):
-				newNode = self.Node(tail)
-				self.graph[tail] = newNode
+                        if m.group(2) == "before":
+                                swap = head
+                                head = tail
+                                tail = swap
 
-			self.graph[head].predArr.append(self.graph[tail])
+                        if not self.graph.__contains__(head):
+                                newNode = self.Node(head)
+                                self.graph[head] = newNode
+
+                        if not self.graph.__contains__(tail):
+                                newNode = self.Node(tail)
+                                self.graph[tail] = newNode
+
+                        self.graph[head].predArr.append(self.graph[tail])
 
 class Usage(Exception):
         def __init__(self, msg):
                 self.msg = msg
 
 def main(argv=None):
-	if argv is None:
- 		argv = sys.argv
+        if argv is None:
+                argv = sys.argv
 
-	parade = Parade()
-	parade.input(argv[1])
-	parade.printGraph()
-	parade.sort()
-	parade.output()
-
-        # parse command line arguments
-        #try:
-       #         try:
-                        # Parse options and args.  If = comes after an option name, that option must get a value.  
-                        # Otherwsie it must be used alone.   This function enforces that.
-                        #opts, args = getopt.getopt(sys.argv[1:], "h", ["help", "accessId", "secretKey=", "hosts="])
-                #except getopt.error, msg:  
-                #        raise Usage(msg)
-
-                # If we need a config file, read it here
-                #print opts
-                #print args
-                #if len(opts) == 0:
-                #        try: 
-                #                print args[0]
-                #        except:
-                #                raise Usage("Please provide either command line options or a config file")
-                #else:
-                #        # process options on commandline, by looping across an array of tuples (o,a) where 
-                        # o is an option name, and v is the value of the option 
-                #        for o,v in opts:
-                #                if o in ("-h", "--help"):
-                #                        print __doc__  # Prevent above help string
-                #                        return 0
-
-                #                if o == "--accessId":
-                #                        accId = a
-                #                elif  o == "--secretKey":
-                #                        secKey = a
-                #                else:
-                #                        hostArr = a.split(',')
-
-
-                # Now we have options, so make sure that we got good ones, or bail.
-                #if accId.__len__() == 0 or secKey.__len__() == 0 or hostArr.__len__() == 0 or hostArr[0].__len__ == 0:
-                #        raise Usage("one or more system parameters missing")
-
-                #poll(accId, secKey, hostArr)
-
-
-        #except Usage, err:
-        #        print >>sys.stderr, err.msg
-        #        print >>sys.stderr, "for help use --help"
-        #        return 2
-
+        parade = Parade()
+        parade.input(argv[1])
+        parade.printGraph()
+        parade.sort()
+        parade.output()
 
 
 if __name__ == "__main__":
